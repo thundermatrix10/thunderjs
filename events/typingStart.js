ec7c71a122cac067c76978198be0e7f10aef9bdf
Aryan Rathod <119438767+thundermatrix10@users.noreply.github.com>
2025-03-25 21:35:07 +0530

    Update typingStart.js

--- events/typingStart.js
+++ events/typingStart.js
@@ -1,21 +1,42 @@
+// const { Events } = require('discord.js');
+
+// module.exports = {
+//     name: Events.TypingStart,
+//     async execute(typing) {
+//         let unread = global.get("unread");
+//         if (unread[typing.user.id] != null) {
+//             let unreadMessages = `Hey <@!${typing.user.id}>! While you were offline, these user[s] have mentioned you, \n\n`;
+
+//             for (let msg in unread[typing.user.id]) {
+//                 msg = unread[typing.user.id][msg];
+//                 unreadMessages += `<@!${msg[0]}> | ${msg[1]}\n`
+//             }
+
+//             unread[typing.user.id] = null;
+//             await typing.channel.send(unreadMessages);
+//             global.set("unread", unread);
+//         }
+//         //console.log(typing.channel.send(`<@!${typing.user.id}>, are you typing?`));
+//     },
+// };
+
 const { Events } = require('discord.js');
 
 module.exports = {
     name: Events.TypingStart,
     async execute(typing) {
-        let unread = global.get("unread");
-        if (unread[typing.user.id] != null) {
-            let unreadMessages = `Hey <@!${typing.user.id}>! While you were offline, these user[s] have mentioned you, \n\n`;
+        let unread = global.get("unread") || {};  // Ensure `unread` is always an object
 
-            for (let msg in unread[typing.user.id]) {
-                msg = unread[typing.user.id][msg];
-                unreadMessages += `<@!${msg[0]}> | ${msg[1]}\n`
+        if (unread[typing.user.id] && unread[typing.user.id].length > 0) {
+            let unreadMessages = `Hey <@!${typing.user.id}>! While you were offline, these user[s] mentioned you:\n\n`;
+
+            for (let msg of unread[typing.user.id]) {
+                unreadMessages += `<@!${msg[0]}> | ${msg[1]}\n`;
             }
 
-            unread[typing.user.id] = null;
+            unread[typing.user.id] = [];  // Clear unread messages after sending
             await typing.channel.send(unreadMessages);
             global.set("unread", unread);
         }
-        //console.log(typing.channel.send(`<@!${typing.user.id}>, are you typing?`));
     },
 };
